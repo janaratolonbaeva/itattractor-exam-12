3685d065b81af21823b77935f576a9764a2348e6
Janara Tolonbaeva <janaratolonbaeva@gmail.com>
2021-05-22 18:13:20 +0600

    modify PhotoCard component file, add route in Link

--- frontend/src/components/PhotoCard/PhotoCard.js
+++ frontend/src/components/PhotoCard/PhotoCard.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import {Link} from "react-router-dom";
 import {
+  Button,
   Card,
   CardActionArea,
   CardActions,
   CardContent,
   CardMedia,
-  Typography,
+  Grid,
   makeStyles,
-  Grid
+  Typography
 } from '@material-ui/core';
 import {apiURL} from "../../config";
 
 const useStyles = makeStyles({
+  root: {
+    marginBottom: '20px'
+  },
   card: {
     width: '90%',
     margin: '0 auto'
@@ -21,31 +25,56 @@ const useStyles = makeStyles({
     height: 0,
     paddingTop: '56.25%',
   },
+  cardActions: {
+    flexDirection: 'column',
+    alignItems: "flex-start"
+  },
+  name: {
+    marginBottom: '20px'
+  }
 });
 
-const PhotoCard = ({onClick, title, image, id, user}) => {
+const PhotoCard = ({title, image, id, name, onClick, userId, onClickOpenModal}) => {
   const classes = useStyles();
 
+  let btnDelete = (
+    <Button variant="contained" color="primary" onClick={onClick}>Delete</Button>
+  );
+
+  if (id !== userId) {
+    btnDelete = null;
+  }
+
   return (
-    <Grid item xs={6} md={4}>
-      <Card className={classes.card}>
-        <CardActionArea onClick={onClick}>
-          <CardMedia
-            className={classes.media}
-            image={apiURL + '/' + image}
-            title={title}
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="h2">
-              {title}
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-        <CardActions>
-          <Typography component={Link} to={'/users/' + id} color="inherit">{user}</Typography>
-        </CardActions>
-      </Card>
-    </Grid>
+    <>
+      <Grid item xs={6} md={4} className={classes.root}>
+        <Card className={classes.card}>
+          <CardActionArea onClick={onClickOpenModal}>
+            <CardMedia
+              className={classes.media}
+              image={apiURL + '/' + image}
+              title={title}
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                {title}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions className={classes.cardActions}>
+            <Typography
+              component={Link}
+              to={'/users/' + id}
+              color="inherit"
+              className={classes.name}
+            >{name}</Typography>
+            <div>
+              {btnDelete}
+            </div>
+          </CardActions>
+        </Card>
+      </Grid>
+    </>
   );
 }
 
